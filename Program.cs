88d431ec88ff3ae630ea4a6bfea2d7e28ee2f584
Corey Schroeder <codingcorey365@gmail.com>
2024-05-04 12:38:03 -0500

    finished WeatherMapApi

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Weather_API
+﻿using System.Runtime.InteropServices.JavaScript;
+using Newtonsoft.Json.Linq;
+
+namespace Weather_API
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your zipcode");
+            string zipcode = Console.ReadLine();
+
+            var apiCall =
+                $"https://api.openweathermap.org/data/2.5/weather?zip={zipcode}&units=imperial&appid={APIKey}";
+            
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
         }
     }
 }
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+/*
+ * latitude and longitude for Belton, Missouri zip 64012
+ * https://api.openweathermap.org/data/2.5/weather?lat=38.8120&lon=94.5319&appid=b26d483ba35fd71e32195fb502724a19
+ */
+
+/*
+ *
+ */
+
+/*
+ *
+ */
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
---
+++ WeatherMap.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Weather_API;
+
+public class WeatherMap
+{
+    public static double GetTemp(string apiCall)
+    {
+        var client = new HttpClient();
+        var response = client.GetStringAsync(apiCall).Result;
+        var temp = double.Parse(JObject.Parse(response)["main"]["temp"].ToString());
+        return temp;
+    }
+}
